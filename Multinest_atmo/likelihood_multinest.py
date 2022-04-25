1bc8f3d096afe2f676e650592dcee49135ea7fc6
Florian Debras <florian_debras@hotmail.com>
2022-04-25 11:37:27 +0200

    Added global beta for gibson likelihood

--- Multinest_atmo/likelihood_multinest.py
+++ Multinest_atmo/likelihood_multinest.py
@@ -6,6 +6,14 @@ import pandas as pd
 
 
 def calc_likelihood(corr,like_type):
+    
+    try :
+        (len(corr["data"]) == len(corr["model"]))
+        (len(corr["data"]) == len(corr["std"]))
+    except:
+        raise NameError("data and model not equal")
+    exit()
+    
     like = np.zeros(len(corr["data"]))
     
     
@@ -13,17 +21,15 @@ def calc_likelihood(corr,like_type):
 
 
     if like_type=="Brogi":
-        try :
-            (len(corr["data"]) == len(corr["model"]))
-        except:
-            raise NameError("data and model not equal")
-            exit()
+
         for i  in range(len(corr["data"])):
             N = len(corr["data"][i])
             sf  = np.var(corr["data"][i])
             sg  = np.var(corr["model"][i])
             Rs = 1./N*np.sum(np.array(corr["data"][i])*np.array(corr["model"][i]))
             like[i]= -N/2.*np.log(sf+sg-2.*Rs)
+            
+        return np.sum(like)
 
     
            
@@ -35,5 +41,20 @@ def calc_likelihood(corr,like_type):
             if tolog<=0.0:
                 print(tolog)
             like[i]= -N/2.*(np.log(tolog))
+            
+        return np.sum(like)
+            
+    elif like_type == "Gibson_global":
+        Ntot = 0
+        for i  in range(len(corr["data"])):
+            N = len(corr["data"][i])
+            Ntot += N
+            like[i] = np.sum((np.array(corr["data"][i])-np.array(corr["model"][i]))**2/np.array(corr["std"][i])**2)
+            if tolog<=0.0:
+                print(tolog)
+                exit()
+            
+        liketot= -Ntot/2.*(np.log(np.sum(like)/Ntot))
+        return liketot        
         
-    return np.sum(like)
+    
